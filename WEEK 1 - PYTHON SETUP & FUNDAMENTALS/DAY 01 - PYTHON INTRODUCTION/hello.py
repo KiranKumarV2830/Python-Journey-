51f69f8542d2005d90c8baf07f7e16ce006dd6cd
Kiran Kumar V <kumarkiran760790@gmail.com>
2026-07-02 08:57:46 +0530

    Python Introduction

---
+++ WEEK 1 - PYTHON SETUP & FUNDAMENTALS/DAY 01 - PYTHON INTRODUCTION/hello.py
@@ -0,0 +1,25 @@
+#Python = It is a programming language which is used for communicating with the computer .
+# Python is one of the easiest language to read because its syntax is close to english.
+
+print("Hello World")
+print("I am Kiran Kumar V")
+print("I want to become AI/ML engineer")
+
+print("Name : Kiran Kumar V")
+print("Dream Company : Google")
+print("Favourite AI : Claude")
+print("Current Year : 2026")
+
+print(" ======= ")
+print("Welcome to my AI journey")
+print(" ======= ")
+
+#Bonus Challenge
+
+print("**************")
+print(" AI JOURNEY ")
+print("**************")
+print("Python")
+print("Machine Learning")
+print("Deep Learning")
+print("**************")
