fe20564bc7d09a2d4f730f4a51cfc370a803fafe
Kiran Kumar V <kumarkiran760790@gmail.com>
2026-07-03 11:14:31 +0530

    Complete Day 2: Variables and data types

---
+++ WEEK 1 - PYTHON SETUP & FUNDAMENTALS/DAY 02 - VARIABLES/day2.py
@@ -0,0 +1,85 @@
+ # Variable = It is a container for storing a value 
+
+ # Variable behaves as if it was the value it contains
+
+ # There are 4 types of variables in python
+ # 1.String
+ # 2.Integer
+ # 3.Float
+ # 4.Boolean
+
+# first_name = "Kiran"
+# food = "pizza"
+# email = "Kiran@qq.com" 
+# print(f"Hello {first_name}");
+# print(f"You like {food}");
+# print(f"Your email is {email}");
+
+# 1 . String = A sequence of characters enclosed by single,double or triple quotes. 
+
+# Name = "Kiran";
+# print(Name);
+
+# 2 . Integers = Represents whole number(Positive or negative) without decimals.
+
+# age = 17;
+# quantity = 3
+# print(f"Your age is {age}");
+# print(f"You are buying {quantity} items");
+
+# 3 . Float = Represents real numbers with decimal points.
+
+# pi = 3.14;
+# print(f"The value of pi is {pi}"); 
+
+# 4 . Boolean = It represents only two values true or false . 
+
+# is_student = True
+# print(type(is_student))
+
+name = "Kiran Kumar V";
+age = 17;
+height = 5.8;
+is_ai_engineer = True;
+
+print(name);
+print(age);
+print(height);
+print(is_ai_engineer);    
+
+print(type(name));
+print(type(age));
+print(type(height));
+print(type(is_ai_engineer));
+
+# Challenge 01 
+
+Dream_company = "Google";
+Favourite_movie = "Dhurandar";
+Favourite_AI = "Chatgpt";
+Years_to_become_aiengineer = 4 ;
+
+print(Dream_company);
+print(Favourite_movie);
+print(Favourite_AI);
+print(Years_to_become_aiengineer);
+
+# Challenge 02 = It prints every name 
+
+name1 = "Kiran"
+print(name1)
+name2 = "KRISHNA"
+print(name2)
+
+# Bonus Challenge
+name3 = "Kiran Kumar V";
+age2 = 18;
+height = 5.8;
+engineer = True
+
+print("======= STUDENT CARD =======");
+print(f"Name : {name3}");
+print(f"Age : {age2}");
+print(f"Height : {height}");
+print(f"AI Engineer : {engineer}");
+print("=============================")
---
+++ WEEK 1 - PYTHON SETUP & FUNDAMENTALS/DAY 02 - VARIABLES/my_profile.py
@@ -0,0 +1,15 @@
+Name = "Kiran Kumar V";
+Age = 17 ;
+City = "Bangalore";
+Dream_Company = "Google";
+Favourite_programming_language = "Python";
+Favourite_AI = "Chatgpt";
+is_student = True;
+
+print(Name,type(Name));
+print(Age,type(Age));
+print(City,type(City));
+print(Dream_Company,type(Dream_Company));
+print(Favourite_programming_language,type(Favourite_programming_language));
+print(Favourite_AI,type(Favourite_AI));
+print(is_student,type(is_student));
