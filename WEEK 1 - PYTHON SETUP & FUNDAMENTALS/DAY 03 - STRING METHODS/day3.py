e7874ebc15e33d6adfec9e78a1b6d2583b57f9f2
Kiran Kumar V <kumarkiran760790@gmail.com>
2026-07-04 12:47:35 +0530

    Complete Day 3: String methods

---
+++ WEEK 1 - PYTHON SETUP & FUNDAMENTALS/DAY 03 - STRING METHODS/day3.py
@@ -0,0 +1,81 @@
+ # String method : A method is like tool attached to an object
+
+# name= "kiran"
+# print(name.upper())
+
+# 1 . upper() : Converts string into uppercase .
+
+# name = "kiran kumar v"
+# print(name.upper())
+
+# 2 . lower() : Converts string into lowercase .
+
+# name2 = "KIRAN KUMAR V"
+# print(name2.lower())
+
+# 3 . strip() : Removes the spaces from beginning and ending .
+
+# text = "   Kiran     "
+# print(text.strip())
+
+# 4 . split() : Breaks a statement into list of words .
+
+# stat = "I am batman I am manic"
+# print(stat.split())
+
+# 5 . replace() : Replaces the word with another word that is defined .
+
+# sentence = "I am spiderman"
+# print(sentence.replace("spiderman", "batman"))
+
+# 6 . len() : Returns the number of the characters present in the string .
+# also they calculate the space as a character .
+
+# mane = "Kiran Kumar V"
+# print(len(mane))
+
+# Combining Methods
+
+# name3 = "         Kiran loves gaming     "
+# print(name3.strip().upper().split())
+# print(name3.strip().lower().split())
+
+name = "Kiran Kumar V"
+print(name.upper())
+print(name.lower())
+
+city = "  Bangalore   "
+print(city.strip())
+
+sentence = "I love Java"
+print(sentence.replace("Java" , "Python"))
+
+skills = "PYTHON AI ML"
+print(skills.split())
+
+print(len(name))
+
+# Challenge 01
+
+quote = "It is our choices, Harry, that show what we truly are, far more than our abilities"
+
+print(quote)
+print(quote.upper())
+print(quote.lower())
+print(len(quote))
+
+# Challenge 02
+
+email = "   Kiran@gmail.com   "
+
+print(email)
+print(email.strip())
+print(email.lower())
+
+# Bonus Challenge
+
+full_name = "Kiran Kumar V "
+
+print(full_name.upper())
+print(full_name.lower())
+print(len(full_name))
---
+++ WEEK 1 - PYTHON SETUP & FUNDAMENTALS/DAY 03 - STRING METHODS/string_practice.py
@@ -0,0 +1,34 @@
+name = "   Kiran Kumar V   "
+dream_company = "   Google Microsoft   "
+favourite_ai = "  Chat gpt  "
+favourite_language = "  Python   Java   "
+print(name.lower())
+print(dream_company.lower())
+
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name.split())
+print(name.replace("V","Venkata H"))
+print(len(name))
+
+print(dream_company.lower())
+print(dream_company.upper())
+print(dream_company.strip())
+print(dream_company.split())
+print(dream_company.replace("Microsoft","Amazon"))
+print(len(dream_company))
+
+print(favourite_ai.lower())
+print(favourite_ai.upper())
+print(favourite_ai.strip())
+print(favourite_ai.split())
+print(favourite_ai.replace("gpt" , "claude"))
+print(len(favourite_ai))
+
+print(favourite_language.lower())
+print(favourite_language.upper())
+print(favourite_language.strip())
+print(favourite_language.split())
+print(favourite_language.replace("Python","j"))
+print(len(favourite_language))
